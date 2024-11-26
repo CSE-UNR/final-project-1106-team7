381459b8d6c0cca7d9713bc94d24afd673e0129c
Chase Neilson <cneilson@unr.edu>
2024-11-26 20:40:57 +0000

    Final Project

--- madlibs.c
+++ madlibs.c
@@ -3,13 +3,40 @@
 // Final Project-Team 7
 
 #include <stdio.h>
-#define FILE "madlib2.txt"
+#define FILENAME "madlib2.txt"
 
-
+void getBlanks(FILE* fin, char* storyBlanks[]);
+int longestLine(FILE* fin); /* Will return length int */
+void getStory(FILE* fin, char* storyText[]); /* 2D array prototypes
+						only have one set of [],
+						I dunno why */
+void promptUser(FILE* fin, char* storyBlanks[], char* storyText[]);
+void displayStory(char storyBlanks[], char storyText[]); /* Doesn't need
+						file pointer or array
+						pointers, data isn't being 
+						changed */
 
 int main(){
-
+	
 	return 0;
 }
 
+void getBlanks(FILE* fin, char* storyBlanks[]){
 
+}
+
+int longestLine(FILE* fin){
+
+}
+
+void getStory(FILE* fin, char* storyText[]){
+
+}
+
+void promptUser(FILE* fin, char* storyBlanks[], char* storyText[]){
+
+}
+
+void displayStory(char storyBlanks[], char storyText[]){
+
+}
