6e041d5a86347f9acbad899baa61074b9d9ea9ff
1106-Tran-Emily <emilytran@unr.edu>
2024-12-05 01:55:22 +0000

    Update madlibs.c
    added scratch work

--- madlibs.c
+++ madlibs.c
@@ -39,3 +39,38 @@ void promptUser(FILE* fin, char storyBlanks[], int columns, int rows, /*
 void displayStory(int columns, int rows, char storyText[][columns]){
 	
 }
+
+//store story and null characters where blanks are
+//but needs to know how many rows
+for(int i = 0; i < numRows; i++) {
+	char strArr[100];
+	if((fscanf(fin, "N\n", &) == 1) || (fscanf(fin, "V\n", &) == 1) || (fscanf(fin, "A\n", &) == 1)) {
+		storyArray[i][0] = '\0';
+	}
+	else {
+		fscanf(fin, "%s", &storyArray[i]);
+	}
+}
+
+//replace null characters with user input
+for(int i = 0; i < numRows; i++) {
+	int j = 0;
+	if(storyArray[i][0] == '\0') {
+		promptUser(j, storyArray[i][MACRO], blanksArray[i]);
+		scanf("%s", &storyArray[i]);
+		j++;
+	}
+}
+
+//prompt user based on what kind of blank needs to be filled
+printf("Please enter a ");
+if(blanksArray[j] == 'A') {
+	printf("adjective: ");
+}
+else if(blanksArray[j] == 'V') {
+	printf("verb: ");
+}
+else if(blanksArray[j] == 'N') {
+	printf("noun: ");
+}
+
