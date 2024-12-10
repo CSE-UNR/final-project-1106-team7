25a3341aca4d4022e204b7d2eae47d50876083a6
1106-Tran-Emily <emilytran@unr.edu>
2024-12-10 00:48:16 +0000

    Update madlibs.c

--- madlibs.c
+++ madlibs.c
@@ -3,62 +3,43 @@
 // Final Project-Team 7
 
 #include <stdio.h>
-
 #define FILENAME "madlib2.txt"
 #define ARBCOL 100
 
-int getRows(FILE* fin);
 void getStory(FILE* fin, int columns, int rows, char storyText[][columns]);
 void promptUser(int columns, int rows, char storyText[][columns]);
 void displayStory(int rows, int colSize, char storyArray[][colSize]);
+int getRows(FILE* fin);
 
 int main(){
 	FILE* fptr;
 	
 	fptr = fopen(FILENAME, "r");
-	
 	if(fptr == NULL){
 		printf("Could not open %s, please try again.\n", FILENAME);
 		return 0;
 	}
 	
-	int rows = getRows(fptr);
+	int totalRows = getRows(fptr);
+	
+	char story[totalRows][ARBCOL];
+	char input[ARBCOL];
 
-	// Initialize story array with row func variable and arbitrary column amount
-	char story[rows][ARBCOL];
-
-	// Open file again to read from start
 	fptr = fopen(FILENAME, "r");
+	if(fptr == NULL){
+		printf("Could not open %s, please try again.\n", FILENAME);
+		return 0;
+	}
 
-	// Run madlibs functions
-	getStory(fptr, ARBCOL, rows, story);
-	promptUser(ARBCOL, rows, story);
-	displayStory(ARBCOL, rows, story);
+	getStory(fptr, ARBCOL, totalRows, story);
+	
+	promptUser(ARBCOL, totalRows, story);
+	
+	displayStory(totalRows, ARBCOL, story);
 	
 	fclose(fptr);
 	
 	return 0;
-}
-
-int getRows(FILE* fin){
-	char test;
-	int rowIndex = 0;
-
-	// Loop counting endline characters
-	while(fscanf(fin, "%c", &test) == 1){
-		if(test == '\n'){
-			rowIndex++;
-		}
-	}
-	
-	return rowIndex;
-}
-
-void getStory(FILE* fin, int columns, int rows, char storyText[][columns]){	
-	// Just grabs strings
-	for(int rowIndex = 0; rowIndex < rows; rowIndex++){
-		fgets(storyText[rowIndex], columns, fin);
-	}
 }
 
 void getStory(FILE* fin, int columns, int rows, char storyText[][columns]) {	
@@ -94,4 +75,18 @@ void displayStory(int rows, int colSize, char storyArray[][colSize]) {
 			printf(" ");
 		}
 	}
+}
+
+int getRows(FILE* fin) {
+	char test;
+	int rowIndex = 0;
+
+	// Loop counting endline characters
+	while(fscanf(fin, "%c", &test) == 1){
+		if(test == '\n'){
+			rowIndex++;
+		}
+	}
+	
+	return rowIndex;
 }
